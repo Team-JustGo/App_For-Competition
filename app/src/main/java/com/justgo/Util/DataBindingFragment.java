375fb93e3857478d26c9eab208ca79b9d7dae9de
이종현 <dwg7691@gmail.com>
2018-09-23 13:42:46 +0900

    Add base class

---
+++ app/src/main/java/com/justgo/Util/DataBindingFragment.java
@@ -0,0 +1,28 @@
+package com.justgo.Util;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class DataBindingFragment<T extends ViewDataBinding> extends Fragment {
+
+    public View view;
+    public T binding;
+
+    public abstract int getLayoutId();
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false);
+        binding.setLifecycleOwner(this);
+        view = binding.getRoot();
+        return view;
+    }
+}
--- app/src/main/java/com/justgo/ui/main/MainActivity.kt
+++ app/src/main/java/com/justgo/ui/main/MainActivity.kt
@@ -2,16 +2,16 @@ package com.justgo.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
-import android.transition.ChangeBounds
 import android.transition.Fade
 import android.transition.TransitionManager
 import android.view.animation.OvershootInterpolator
 import com.justgo.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_backdrop.*
+import kotlinx.android.synthetic.main.main_toolbar.*
 import org.jetbrains.anko.find
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     val travelCardView by lazy { find<ConstraintLayout>(R.id.main_startTravel_constraint) }
 
     var isTravelStart = false
+    var isBackdropOpened = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,11 +32,28 @@ class MainActivity : AppCompatActivity() {
                 updateConstraints(R.layout.activity_main_travel, container)
                 updateConstraints(R.layout.activity_main_travel, travelCardView)
                 isTravelStart = true
+                isBackdropOpened = false
             } else {
                 updateConstraints(R.layout.activity_main, container)
                 updateConstraints(R.layout.activity_main_travel, travelCardView)
                 isTravelStart = false
+                isBackdropOpened = false
             }
+        }
+
+        main_toolbar_list_iv.onClick {
+            if (!isBackdropOpened) {
+                updateConstraints(R.layout.activity_main_backdrop, container)
+                isBackdropOpened = true
+            } else {
+                updateConstraints(R.layout.activity_main_travel, container)
+                isBackdropOpened = false
+            }
+        }
+
+        main_backdrop_up_iv.onClick {
+            updateConstraints(R.layout.activity_main_travel, container)
+            isBackdropOpened = false
         }
 
     }
