af30450701f308c5f132d5ae8273c607961393d5
Woopa01 <whdnwls49@naver.com>
2018-09-28 23:30:16 +0900

    end

---
+++ app/src/main/java/com/justgo/ui/SelectTrip/SelectTripActivity.kt
@@ -0,0 +1,14 @@
+package com.justgo.ui.SelectTrip
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.justgo.R
+
+class SelectTripActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_trip)
+
+
+    }
+}
