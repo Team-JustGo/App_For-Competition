eb195635e6d2c038a3a9865b91cd2cdec6490b08
이종현 <dwg7691@gmail.com>
2018-09-29 02:29:50 +0900

    Add Navigation xml

---
+++ app/src/main/java/com/justgo/ui/navigation/NavigationActivity.kt
@@ -0,0 +1,13 @@
+package com.justgo.ui.navigation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.justgo.R
+
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
