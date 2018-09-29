938390a2dc60155c72b59c67711961a1fa2687f9
이종현 <dwg7691@gmail.com>
2018-09-30 01:29:08 +0900

    Add Map

--- app/src/main/java/com/justgo/ui/navigation/NavigationActivity.kt
+++ app/src/main/java/com/justgo/ui/navigation/NavigationActivity.kt
@@ -1,13 +1,50 @@
 package com.justgo.ui.navigation
 
+import android.Manifest
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.justgo.R
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.support.v4.content.ContextCompat
 
-class NavigationActivity : AppCompatActivity() {
+
+class NavigationActivity : AppCompatActivity(), OnMapReadyCallback {
+    var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.main_startTravel_mapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.uiSettings.isScrollGesturesEnabled = false
+        map.uiSettings.isZoomGesturesEnabled = false
+        if (ContextCompat.checkSelfPermission(baseContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10F, object : LocationListener {
+                override fun onLocationChanged(p0: Location?) {
+                    
+                }
+
+                override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+                }
+
+                override fun onProviderEnabled(p0: String?) {
+                }
+
+                override fun onProviderDisabled(p0: String?) {
+                }
+
+            })
     }
 }
